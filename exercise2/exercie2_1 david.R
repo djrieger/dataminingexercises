34b8a445c3a75a0e538f690810cb10874b3eca72
David Rieger <djrieger@users.noreply.github.com>
2014-11-17 18:08:56 +0100

    Completed exercise2/1 (still contains errors)

--- exercise2/exercie2_1 david.R
+++ exercise2/exercie2_1 david.R
@@ -1,39 +1,54 @@
-myFunction <- function(data) 
+myFunction <- function(data, class = NULL) 
 { 
-  attribute_names <- names(mydata)
-  B = matrix( 
-       #c("col1", "col2", "cor"), 
-       nrow=1, 
-       ncol=3) 
-  for(i in 1:length(attribute_names)) {
-    x <- attribute_names[i]
-    #print(x)
-    column <- mydata[[x]]
-    
-    if (is.numeric(column)) {
-      png(filename=paste("Histograms/", x, ".png", sep = ""))
-      hist(column, main = x)
-      dev.off()
+  if (is.null(class)) {
+    attribute_names <- names(data)
+    B = matrix( 
+      #c("col1", "col2", "cor"), 
+      nrow=1, 
+      ncol=3) 
+    for(i in 1:length(attribute_names)) {
+      x <- attribute_names[i]
+      #print(x)
+      column <- data[[x]]
       
-      for(j in (i+1):length(attribute_names)) {
-        y <- attribute_names[j]
-        column2 <- mydata[[y]]
-        if (!(x == y) && is.numeric(column2) && !(x == "points") && !(y == "points")) {
-          B = rbind(B, c(x, y, abs(cor(column, column2))))
-          #print(paste("--", y, ": ", cor(column, column2)))
+      if (is.numeric(column)) {
+        png(filename=paste("Histograms/", x, ".png", sep = ""))
+        hist(column, main = x)
+        dev.off()
+        
+        for(j in (i+1):length(attribute_names)) {
+          y <- attribute_names[j]
+          column2 <- data[[y]]
+          if (!(x == y) && is.numeric(column2) && !(x == "points") && !(y == "points")) {
+            B = rbind(B, c(x, y, abs(cor(column, column2))))
+            #print(paste("--", y, ": ", cor(column, column2)))
+          }
         }
       }
     }
-  }
-  B = B[order(as.numeric(B[,3])),]
-  for (i in 1:3) {
-    print(B[i,])
-    plot(mydata[[B[i,1]]], mydata[[B[i,2]]], xlab=B[i,1], ylab=B[i,2])
-    row <- nrow(B) - 4 + i
-    print(B[row,])
-    plot(mydata[[B[row,1]]], mydata[[B[row,2]]], xlab=B[row,1], ylab=B[row,2])
+    
+  } else {
+    for (uniqueVal in unique(data[[class]])) {
+      print(uniqueVal)
+      part <- subset(data, data[[class]] == uniqueVal)
+      hist(part[[class]])
+    }
+    
+    return
+    
+    # sort B and plot
+    B = B[order(as.numeric(B[,3])),]
+    for (i in 1:3) {
+      print(B[i,])
+      plot(data[[B[i,1]]], data[[B[i,2]]], xlab=B[i,1], ylab=B[i,2])
+    }
+    for (i in 1:3) {
+      row <- nrow(B) - 4 + i
+      print(B[row,])
+      plot(data[[B[row,1]]], data[[B[row,2]]], xlab=B[row,1], ylab=B[row,2])
+    }
   }
 }
 
 mydata <- read.csv("dmc2010_train.txt",header = TRUE, sep = ";", quote = "\"")
-myFunction(mydata)
+myFunction(mydata, "model")
