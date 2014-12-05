67c8fdca44cd1b22d586b70625848c7866ba40a2
Sebastian Bittel <sebastian.bittel@gmail.com>
2014-12-05 16:28:30 +0100

    ex3

---
+++ exiercise3/Exercise3_1_SC.R
@@ -0,0 +1,28 @@
+
+
+calculateSH <- function(clustervector, data){
+  
+  
+
+
+for i in data$clusters
+
+# calculate a(o) average distance between object o and the objects in its cluster A
+  a = 1/ data_cluster1 sum(dist())
+# calculate b(o) average dostance between object o and objects in its second closest cluster B
+
+  b = min(1/)
+  
+
+# calcualte silhouette of o
+s = 0
+if(a != 0) {
+  s = b -a / (max{a,b})
+}
+
+result = 1/size of cluster sum(1/specic cluster )* s(o)
+  
+}
+
+
+
